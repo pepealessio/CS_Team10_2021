e3cce84fe68310f95d921e54a8c75fa182a90d9c
Alessio Pepe <pepealessio.ap@gmail.com>
2021-06-13 12:38:46 +0200

    fix thread

--- src/main/java/it/unisa/diem/cs/gruppo10/User.java
+++ src/main/java/it/unisa/diem/cs/gruppo10/User.java
@@ -10,6 +10,7 @@ import java.security.spec.ECGenParameterSpec;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Ths class is used to simulate an user in the CT system.
@@ -56,19 +57,23 @@ public class User implements Serializable {
     public static void meet2user(User u1, User u2) throws InterruptedException {
         Thread u1tou2 = new Thread(() -> {
             try {
-                u2.receiveContact(u1);
+                u1.receiveContact();
+                TimeUnit.MILLISECONDS.sleep(300);
+                u1.sendContact(u2);
             } catch (Exception ignored) {
             }
         });
 
         Thread u2tou1 = new Thread(() -> {
             try {
-                u1.sendContact(u2);
+                u2.sendContact(u1);
+                u2.receiveContact();
             } catch (Exception ignored) {
             }
         });
 
         u1tou2.start();
+        TimeUnit.MILLISECONDS.sleep(300);
         u2tou1.start();
 
         u1tou2.join();
@@ -104,37 +109,22 @@ public class User implements Serializable {
         // Start u1 soket as pair BT request
         Socket clientSocket = new Socket("127.0.0.1", u2.getPort());
         ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
-        ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
 
         // Now we simulate contact excange ----------------------------------------------------
         // User 1 send message to user 2 as (2.6)
         ContactMessage c1to2 = new ContactMessage(keyPairF, u2.getId());
         out.writeObject(c1to2);
 
-        try {
-            ContactMessage c2to1 = (ContactMessage) in.readObject();
-            if (c2to1.verify(getId())) {
-                System.out.println(name + ": I've added a contact");
-                contacts.add(c2to1);
-            } else {
-                // If verify not have success
-                System.out.println(name + ": I've REJECTED a contact");
-            }
-        } catch (ClassNotFoundException ignored) {
-        }
-
         // Simulate Two user close connection ------------------------------------------------
-        in.close();
         out.close();
         clientSocket.close();
     }
 
-    private void receiveContact(User u1) throws IOException, ClassNotFoundException, NoSuchAlgorithmException, SignatureException, InvalidKeyException {
+    private void receiveContact() throws IOException, ClassNotFoundException, NoSuchAlgorithmException, SignatureException, InvalidKeyException {
         // SIMULATE PAIRING ------------------------------------------------------------------
         // Start u2 soket as pair BT accepting
         ServerSocket serverSocket = new ServerSocket(port);
         Socket clientSocket = serverSocket.accept();
-        ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
         ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
 
         // Now we simulate contact excange ----------------------------------------------------
@@ -143,11 +133,6 @@ public class User implements Serializable {
         if (c1to2.verify(getId())) {
             System.out.println(name + ": I've added a contact");
             contacts.add(c1to2);
-
-            // If verify have success
-            ContactMessage c2to1 = new ContactMessage(keyPairF, u1.getId());
-            out.writeObject(c2to1);
-
         } else {
             // If verify not have success
             System.out.println(name + ": I've REJECTED a contact");
@@ -155,19 +140,8 @@ public class User implements Serializable {
 
         // Simulate Two user close connection ------------------------------------------------
         in.close();
-        out.close();
         clientSocket.close();
         serverSocket.close();
-    }
-
-    private void communicatePositivity()
-    {
-
-    }
-
-    private void getNotify()
-    {
-
     }
 
     public static void main(String[] args) throws InvalidAlgorithmParameterException, NoSuchAlgorithmException, InterruptedException {
